75955e71530f89e7e06679283ca956f11504fdc4
Marco Vanali <Marco91Vanali@gmail.com>
2018-02-11 21:37:45 +0000

    Index.js, the web socket server, on connection, listen for join and message message

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const server = require('http').Server(app) //run the server on app
 const io = require('socket.io')(server); //run our server on socket.io
 
 const port = 3000;
+server.listen(port, ()=> {
+  console.log(`Server is listening on Port: ${port}`)
+});
 
 app.get('/', (req, res)=> {
   res.sendFile(__dirname + '/public/index.html')
@@ -20,6 +23,23 @@ app.get('/swift', (req, res)=> {
   res.sendFile(__dirname + '/public/swift.html')
 });
 
-server.listen(port, ()=> {
-  console.log(`Server is listening on Port: ${port}`)
+const tech = io.of('/tech');
+
+tech.on('connection', (socket)=>{
+  // console.log('A user connected')
+  socket.on('join', (data) => {
+    socket.join(data.room);
+    tech.in(data.room).emit('message', `New User Joiner ${data.room} root!`)
+  });
+
+  socket.on('message', (data)=> {
+    tech.in(data.room).emit('message', data.msg);
+    console.log(data)
+  });
+
+  io.on('disconnect', ()=> {
+    console.log('User Disconnected')
+    tech.emit('message', 'User Disconnected')
+  })
+
 });
