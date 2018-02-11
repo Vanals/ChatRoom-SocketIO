4ed80ff65d531a7eafece408ff6bc9923d399d8b
Marco Vanali <Marco91Vanali@gmail.com>
2018-02-11 13:00:09 +0000

    Html ancor now links you to the corrects routes.
    Added bootstrap code in javascript.html

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const port = 3000;
 
 app.get('/', (req, res)=> {
   res.sendFile(__dirname + '/public/index.html')
-})
+});
+
+app.get('/phyton', (req, res)=> {
+  res.sendFile(__dirname + '/public/phyton.html')
+});
+
+app.get('/javascript', (req, res)=> {
+  res.sendFile(__dirname + '/public/javascript.html')
+});
+
+app.get('/swift', (req, res)=> {
+  res.sendFile(__dirname + '/public/swift.html')
+});
 
 server.listen(port, ()=> {
   console.log(`Server is listening on Port: ${port}`)
-})
+});
